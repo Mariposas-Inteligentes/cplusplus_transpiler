25358e6dabc6101964fb1142aea05f3dbad64b0c
Angie Solis <angie.solismanzano@ucr.ac.cr>
2024-09-25 15:56:34 -0600

    Added class test code for tokens

--- src/token_tests.py
+++ src/token_tests.py
@@ -38,3 +38,96 @@ print("Approximation of pi = " + str(pi_value))
     'CLOSED_PARENTHESIS', 'NEWLINE', 'ENDMARKER']
 
     assert token_types == expected
+
+
+def test_correct_class_and_try_code(lexer):
+    code = '''
+class Triangle:
+    def __init__(self, side_a, side_b, side_c):
+        self.side_a = side_a
+        self.side_b = side_b
+        self.side_c = side_c
+        self.triangle_type = None
+        self.invalid = False
+    def classify_triangle(self):
+        if not(self.side_a >= 0 and self.side_b >= 0 and self.side_c >= 0):
+            raise ValueError("Sides must be greater than zero.")
+        if (self.side_a + self.side_b <= self.side_c or
+            self.side_a + self.side_c <= self.side_b or
+            self.side_b + self.side_c <= self.side_a):
+            self.invalid = True
+            self.triangle_type = "Not a triangle"
+        elif self.side_a == self.side_b and self.side_b == self.side_c:
+            self.triangle_type = "Equilateral"
+        elif (self.side_a == self.side_b or 
+              self.side_b == self.side_c or 
+              self.side_a == self.side_c):
+            self.triangle_type = "Isosceles"
+        else:
+            self.triangle_type = "Scalene"
+        return self.triangle_type
+try:
+    sides = [3, 4, 5]
+    results = {'Scalene': 0, 'Equilateral': 0, 'Isosceles': 0}
+    while(True):
+        triangle = Triangle(sides[0], sides[1], sides[2])
+        result = triangle.classify_triangle()
+        if (result != "Not a triangle"):
+            results[result] += 1
+            if results[result] > 2:
+                break
+        sides[0] += 1
+        sides[1] += 1
+        sides[2] += 1
+except ValueError as e:
+    print("Error:", e)
+'''
+    lexer.input(code)
+    token_types = [token.type for token in lexer.token_stream]
+
+    expected = ['CLASS', 'VAR_FUNC_NAME', 'COLON', 'NEWLINE', 'INDENT', 'DEF', 'VAR_FUNC_NAME', 
+    'OPEN_PARENTHESIS', 'VAR_FUNC_NAME', 'COMMA', 'VAR_FUNC_NAME', 'COMMA', 'VAR_FUNC_NAME', 'COMMA', 
+    'VAR_FUNC_NAME', 'CLOSED_PARENTHESIS', 'COLON', 'NEWLINE', 'INDENT', 'VAR_FUNC_NAME', 'PERIOD', 
+    'VAR_FUNC_NAME', 'ASSIGN', 'VAR_FUNC_NAME', 'NEWLINE', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 
+    'ASSIGN', 'VAR_FUNC_NAME', 'NEWLINE', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'ASSIGN', 
+    'VAR_FUNC_NAME', 'NEWLINE', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'ASSIGN', 'NONE', 'NEWLINE', 
+    'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'ASSIGN', 'FALSE', 'NEWLINE', 'DEDENT', 'DEF', 
+    'VAR_FUNC_NAME', 'OPEN_PARENTHESIS', 'VAR_FUNC_NAME', 'CLOSED_PARENTHESIS', 'COLON', 'NEWLINE', 
+    'INDENT', 'IF', 'NOT', 'OPEN_PARENTHESIS', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'MORE_EQUALS', 
+    'INT', 'AND', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'MORE_EQUALS', 'INT', 'AND', 'VAR_FUNC_NAME', 
+    'PERIOD', 'VAR_FUNC_NAME', 'MORE_EQUALS', 'INT', 'CLOSED_PARENTHESIS', 'COLON', 'NEWLINE', 'INDENT', 
+    'VAR_FUNC_NAME', 'VAR_FUNC_NAME', 'OPEN_PARENTHESIS', 'STRING', 'CLOSED_PARENTHESIS', 'NEWLINE', 
+    'DEDENT', 'IF', 'OPEN_PARENTHESIS', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'PLUS', 
+    'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'LESS_EQUALS', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 
+    'OR', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'PLUS', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 
+    'LESS_EQUALS', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'OR', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 
+    'PLUS', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'LESS_EQUALS', 'VAR_FUNC_NAME', 'PERIOD', 
+    'VAR_FUNC_NAME', 'CLOSED_PARENTHESIS', 'COLON', 'NEWLINE', 'INDENT', 'VAR_FUNC_NAME', 'PERIOD', 
+    'VAR_FUNC_NAME', 'ASSIGN', 'TRUE', 'NEWLINE', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'ASSIGN', 'STRING', 
+    'NEWLINE', 'DEDENT', 'ELIF', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'EQUALS', 'VAR_FUNC_NAME', 'PERIOD', 
+    'VAR_FUNC_NAME', 'AND', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'EQUALS', 'VAR_FUNC_NAME', 'PERIOD', 
+    'VAR_FUNC_NAME', 'COLON', 'NEWLINE', 'INDENT', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'ASSIGN', 'STRING', 
+    'NEWLINE', 'DEDENT', 'ELIF', 'OPEN_PARENTHESIS', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'EQUALS', 
+    'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'OR', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'EQUALS', 
+    'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'OR', 'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'EQUALS', 
+    'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'CLOSED_PARENTHESIS', 'COLON', 'NEWLINE', 'INDENT', 'VAR_FUNC_NAME', 
+    'PERIOD', 'VAR_FUNC_NAME', 'ASSIGN', 'STRING', 'NEWLINE', 'DEDENT', 'ELSE', 'COLON', 'NEWLINE', 'INDENT', 
+    'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'ASSIGN', 'STRING', 'NEWLINE', 'DEDENT', 'RETURN', 'VAR_FUNC_NAME', 
+    'PERIOD', 'VAR_FUNC_NAME', 'NEWLINE', 'DEDENT', 'DEDENT', 'TRY', 'COLON', 'NEWLINE', 'INDENT', 'VAR_FUNC_NAME', 
+    'ASSIGN', 'OPEN_BRACKET', 'INT', 'COMMA', 'INT', 'COMMA', 'INT', 'CLOSED_BRACKET', 'NEWLINE', 'VAR_FUNC_NAME', 
+    'ASSIGN', 'OPEN_CURLY_BRACKET', 'STRING', 'COLON', 'INT', 'COMMA', 'STRING', 'COLON', 'INT', 'COMMA', 'STRING', 
+    'COLON', 'INT', 'CLOSED_CURLY_BRACKET', 'NEWLINE', 'WHILE', 'OPEN_PARENTHESIS', 'TRUE', 'CLOSED_PARENTHESIS', 
+    'COLON', 'NEWLINE', 'INDENT', 'VAR_FUNC_NAME', 'ASSIGN', 'VAR_FUNC_NAME', 'OPEN_PARENTHESIS', 'VAR_FUNC_NAME', 
+    'OPEN_BRACKET', 'INT', 'CLOSED_BRACKET', 'COMMA', 'VAR_FUNC_NAME', 'OPEN_BRACKET', 'INT', 'CLOSED_BRACKET', 'COMMA', 
+    'VAR_FUNC_NAME', 'OPEN_BRACKET', 'INT', 'CLOSED_BRACKET', 'CLOSED_PARENTHESIS', 'NEWLINE', 'VAR_FUNC_NAME', 'ASSIGN', 
+    'VAR_FUNC_NAME', 'PERIOD', 'VAR_FUNC_NAME', 'OPEN_PARENTHESIS', 'CLOSED_PARENTHESIS', 'NEWLINE', 'IF', 
+    'OPEN_PARENTHESIS', 'VAR_FUNC_NAME', 'DIFFERENT', 'STRING', 'CLOSED_PARENTHESIS', 'COLON', 'NEWLINE', 'INDENT', 
+    'VAR_FUNC_NAME', 'OPEN_BRACKET', 'VAR_FUNC_NAME', 'CLOSED_BRACKET', 'PLUS_EQUALS', 'INT', 'NEWLINE', 'IF', 
+    'VAR_FUNC_NAME', 'OPEN_BRACKET', 'VAR_FUNC_NAME', 'CLOSED_BRACKET', 'MORE', 'INT', 'COLON', 'NEWLINE', 'INDENT', 
+    'BREAK', 'NEWLINE', 'DEDENT', 'DEDENT', 'VAR_FUNC_NAME', 'OPEN_BRACKET', 'INT', 'CLOSED_BRACKET', 'PLUS_EQUALS', 
+    'INT', 'NEWLINE', 'VAR_FUNC_NAME', 'OPEN_BRACKET', 'INT', 'CLOSED_BRACKET', 'PLUS_EQUALS', 'INT', 'NEWLINE', 
+    'VAR_FUNC_NAME', 'OPEN_BRACKET', 'INT', 'CLOSED_BRACKET', 'PLUS_EQUALS', 'INT', 'NEWLINE', 'DEDENT', 'DEDENT', 
+    'EXCEPT', 'VAR_FUNC_NAME', 'VAR_FUNC_NAME', 'VAR_FUNC_NAME', 'COLON', 'NEWLINE', 'INDENT', 'PRINT', 'OPEN_PARENTHESIS', 
+    'STRING', 'COMMA', 'VAR_FUNC_NAME', 'CLOSED_PARENTHESIS', 'NEWLINE', 'DEDENT', 'ENDMARKER']
+
+    assert token_types == expected
