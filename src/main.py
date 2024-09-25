3baabb9971162d5c2c3aa6f09c7e978a9178af5c
GoninDS <luisdi2509@gmail.com>
2024-09-25 15:43:41 -0600

    Changed main to only allow files with a .py extension

--- src/main.py
+++ src/main.py
@@ -6,8 +6,10 @@ def main():
     if (len(sys.argv) < 2):
         print('Please insert a .py file to transpile')
         return
-    
     file_name = sys.argv[1]
+    if (file_name[len(file_name) - 3:] != ".py"):
+        print('Please insert a file with a .py extension')
+
     transpiler = Transpiler(file_name=file_name, debug=True)
     transpiler.input()
 
