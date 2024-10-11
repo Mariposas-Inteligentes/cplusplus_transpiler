c661fdf7efe40474847c02e275d21fbf9ee6241d
Angie Solis <angie.solismanzano@ucr.ac.cr>
2024-10-11 11:23:11 -0600

    Debug parameter implemented.
    Co-authored-by: emilia_viq <EmiliaViq@users.noreply.github.com>
    Co-Authored-By: Luis Solano <87628394+GoninDS@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -3,16 +3,21 @@ import sys
 
 
 def main():
+    debug = False
+
     if (len(sys.argv) < 2):
         print('Please insert a .py file to transpile')
         return
+    if len(sys.argv) == 3:
+        if (sys.argv[2] == "debug"):
+            debug = True
     
     file_name = sys.argv[1]
     if (file_name[len(file_name) - 3:] != ".py"):
         print('Please insert a file with a .py extension')
         return
 
-    transpiler = Transpiler(file_name=file_name, debug=True)
+    transpiler = Transpiler(file_name=file_name, debug=debug)
     transpiler.input()
 
 if __name__ == "__main__":
