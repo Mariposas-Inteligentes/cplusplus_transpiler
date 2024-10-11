8a644a8ce069a730ff1bcea238199c195422c811
EMILIA MARIA VIQUEZ MORA <emilia.viquez@ucr.ac.cr>
2024-10-11 14:59:15 -0600

    Added more expressions to the math operations test

--- src/tests/parser_test_files/test_math_operations.py
+++ src/tests/parser_test_files/test_math_operations.py
@@ -3,3 +3,8 @@ def my_function():
     y = 2 * (3 + 4)
     z = 10 // 3
     y1 = 2 * 'holiii'
+    y2 = 2 * 'holiii' + y1 * (5 + 4 - 3) // 59 - "string"
+    y3 = 2 * 'holiii' + y1 * (5 + 4 - 3) // 59 - "string" ** 34 - 32 + hola[4] - hola2["wiiiii"] // hola3[hola2["wooooo"][hola1[4]]]
+    y4 += 2 * 'holiii' + y1 * (5 + 4 - 3) // 59 - "string" ** 34 - 32 + hola[4] - hola2["wiiiii"] // hola3[hola2["wooooo"][hola1[4]]] + y2 - y4 
+    # TODO(nosotros): revisitar
+    y5 += 2 * 'holiii' + y1 * (5 + 4 - 3) // 59 - "string" ** yo.pepito.luis.jimena().pedro[pancho["22222"].variables]
